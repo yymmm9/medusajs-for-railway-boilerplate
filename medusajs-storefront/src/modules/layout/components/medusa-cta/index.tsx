fd06c352edf073884f1aa27905156afa4521ecea
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2024-06-29 09:44:17 +0200

    Update index.tsx

--- medusajs-storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ medusajs-storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -1,8 +1,5 @@
 import { Text } from "@medusajs/ui"
 
-import Medusa from "../../../common/icons/medusa"
-import NextJs from "../../../common/icons/nextjs"
-
 const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
